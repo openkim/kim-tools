e78b8e4f2739ea1d6fb005e082982ae5eb751655
ilia Nikiforov <nikif002@umn.edu>
2024-03-23 01:48:31 +0000

    basic rename

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-# kim-python-utils
+# kim-tests
 
-Helper routines for writing KIM Tests and Verification Checks
-
-We are working on submitting this package to PyPI, but in the meantime it can be installed directly from GitHub via:
-```
-pip install --user git+https://github.com/openkim/kim-python-utils
-```
-
-### Looking for the query module?
-
-The kim_python_utils.query module has been moved to its own repo at https://github.com/openkim/kim-query
+KIMTestDriver and CrystalGenomeTestDriver classes for creating OpenKIM Test Drivers
--- setup.py
+++ setup.py
@@ -4,23 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="kim-python-utils",
-    version="0.3.0",
+    name="kim-test-utils",
+    version="0.1.0",
     description=(
-        "Helper routines for writing KIM Tests and Verification Checks"
+        "Helper routines for writing KIM Tests"
     ),
-    author=["Ellad B. Tadmor", "Daniel S. Karls"],
-    url="https://github.com/openkim/kim-python-utils",
+    author=["ilia Nikiforov, Eric Fuemmler"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     license="CDDL",
-    install_requires=["numpy >= 1.13.1", "scipy >= 1.3.0", "jinja2 >= 2.7.2", "ase >= 3.19.0b1", "kim-property >= 2.5.8"],
-    python_requires=">=2.7",
+    install_requires=["ase >= 3.19.0b1", "kim-property >= 2.5.8", "crystal-genome-util"],
     classifiers=[
         "Development Status :: 4 - Beta"
         "License :: OSI Approved :: Common Development and Distribution License 1.0 (CDDL-1.0)",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Physics",
