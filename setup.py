b170952dd2338e19f30f0fc3409534220414be54
ilia Nikiforov <nikif002@umn.edu>
2024-06-17 00:24:20 +0000

    changing setup

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 import setuptools
+import subprocess
+from warnings import warn
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+try:
+    subprocess.check_output('aflow --proto=A_cF4_225_a')
+except Exception:
+    warn("aflow executable not found in PATH. You will not be able to run any Crystal Genome tests.")
 
 setuptools.setup(
     name="kim-test-utils",
@@ -14,7 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     license="CDDL",
-    install_requires=["ase >= 3.19.0b1", "kim-property >= 2.5.8"],
+    install_requires=["ase >= 3.19.0b1", "kim-property >= 2.6.2", "kim-edn >= 1.4.1", "kimpy >= 2.1.1", "spglib >= 2.1.0"],
     classifiers=[
         "Development Status :: 4 - Beta"
         "License :: OSI Approved :: Common Development and Distribution License 1.0 (CDDL-1.0)",
