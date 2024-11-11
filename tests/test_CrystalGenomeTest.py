fa3618ae9bede52eb0b818799551a3866f2399fe
ilia Nikiforov <nikif002@umn.edu>
2024-11-10 18:36:40 -0600

    enable finding local props automatically and add tests

--- tests/test_CrystalGenomeTest.py
+++ tests/test_CrystalGenomeTest.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 from kim_tools.test_driver import CrystalGenomeTestDriver, query_crystal_genome_structures
-from kim_python_utils.ase import get_isolated_energy_per_atom
 from kim_tools.aflow_util import get_stoich_reduced_list_from_prototype
 
 class TestTestDriver(CrystalGenomeTestDriver):
@@ -9,18 +8,12 @@ class TestTestDriver(CrystalGenomeTestDriver):
         """
         example calculate method. Just writes the binding-energy and crystal-structure-npt properties assuming the provided
         structure is already at equilibrium
-
-        Args:
-            structure_index:
-                KIM tests can loop over multiple structures (i.e. crystals, molecules, etc.). This indicates which is being used for the current calculation.        
         """
         # calculate potential energy and do the required stuff to figure out per-formula and per-atom, and subtract isolated energy
         potential_energy = self.atoms.get_potential_energy()
         potential_energy_per_atom = potential_energy/len(self.atoms)
         reduced_stoichiometry = get_stoich_reduced_list_from_prototype(self.prototype_label) # i.e. "AB3\_...." -> [1,3]        
         binding_energy_per_formula = potential_energy_per_atom * sum(reduced_stoichiometry)
-        for num_in_formula,species in zip(reduced_stoichiometry,self.stoichiometric_species):
-            binding_energy_per_formula -= num_in_formula*get_isolated_energy_per_atom(self.kim_model_name,species)
         binding_energy_per_atom = binding_energy_per_formula/sum(reduced_stoichiometry)
 
         # add property instance and common fields
@@ -35,11 +28,10 @@ class TestTestDriver(CrystalGenomeTestDriver):
             f.write("dummy\nfile")
         self._add_file_to_current_property_instance("restart-file","restart.dump")
 
-        
-test = TestTestDriver("MEAM_LAMMPS_KoJimLee_2012_FeP__MO_179420363944_002")
-list_of_crystal_descriptions = query_crystal_genome_structures("MEAM_LAMMPS_KoJimLee_2012_FeP__MO_179420363944_002",["Fe","P"],"AB_oP8_62_c_c")
-print(list_of_crystal_descriptions)
-test(**list_of_crystal_descriptions[0])
-test(**list_of_crystal_descriptions[1])
-print(test.property_instances)
-test.write_property_instances_to_file()
+
+def test_cg():        
+    test = TestTestDriver("LJ_Shifted_Bernardes_1958LowCutoff_Ar__MO_720819638419_004")
+    list_of_crystal_descriptions = query_crystal_genome_structures("LJ_Shifted_Bernardes_1958LowCutoff_Ar__MO_720819638419_004",["Ar"],"A_hP2_194_c")
+    test(**list_of_crystal_descriptions[0])    
+    assert len(test.property_instances) == 2
+    test.write_property_instances_to_file()
--- tests/test_KIMTest.py
+++ tests/test_KIMTest.py
@@ -2,19 +2,37 @@
 
 from kim_tools.test_driver import KIMTestDriver
 from ase.atoms import Atoms
+from ase.calculators.lj import LennardJones
 
 class TestTestDriver(KIMTestDriver):
-    def _calculate(self):
+    def _calculate(self,property_name):
         """
         example calculate method
+        
+        Args:
+            property_name: for testing ability to find properties at different paths.
+            !!! AN ACTUAL TEST DRIVER SHOULD NOT HAVE AN ARGUMENT SUCH AS THIS !!!
         """
         self.species = self.atoms.get_chemical_symbols()[0]
-        self._add_property_instance("atomic-mass","This is an example disclaimer.")
+        self._add_property_instance(property_name,"This is an example disclaimer.")
         self._add_key_to_current_property_instance("species", self.atoms.get_chemical_symbols()[0])
         self._add_key_to_current_property_instance("mass", self.atoms.get_masses()[0], "amu", {'source-std-uncert-value':1})
 
-atoms = Atoms(['Ar'], [[0, 0, 0]], cell=[[1, 0, 0], [0, 2, 0], [0, 0, 2]])
-test = TestTestDriver("LennardJones_Ar")
-test(atoms)
-test.write_property_instances_to_file()
-print(test.property_instances)
+def test_kimtest():
+    atoms = Atoms(['Ar'], [[0, 0, 0]], cell=[[1, 0, 0], [0, 2, 0], [0, 0, 2]])
+    test = TestTestDriver(LennardJones())
+    testing_property_names = [
+        'atomic-mass', # already in kim-properties
+        'atomic-mass0', # found in $PWD/local-props
+        'atomic-mass0', # check that repeat works fine
+        'tag:brunnels@noreply.openkim.org,2016-05-11:property/atomic-mass1', # check that full id works as well, found in $PWD/local-props
+        'atomic-mass2', # found in $PWD/local-props/atomic-mass2
+        'atomic-mass3', # found in $PWD/mock-test-drivers-dir/mock-td/local_props. For testing how KDP will set these.
+                        # test this using `export KIM_PROPERTY_PATH=$PWD/mock-test-drivers-dir/*/local-props:$PWD/mock-test-drivers-dir/*/local_props`
+    ]
+
+    for prop_name in testing_property_names:
+        test(atoms,property_name=prop_name)
+        
+    assert len(test.property_instances) == 6
+    test.write_property_instances_to_file()
