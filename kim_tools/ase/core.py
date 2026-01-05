1d7648c05ce887b43c3140fb6667693b769f0e07
nav-mohan <mohan227@umn.edu>
2026-01-04 23:33:55 -0600

    ISSUE #24 find working configuration
    A new computational protocol for finding a working configuration FCC for a given model and species
    For a given model and species list, a configuration is determined by the lattice-constant of the FCC structure.
    A configuration is considered "good" if it satisfies the following
    - FCC structure's energy is bounded
    - FCC structure's lattice constant is small enough to guarantee interaction between all atoms but larger than the minimum pairwise cutoff distance.
    - energy vs lattice-constant curve is smooth in the vicinity of the "good" lattice-constant

--- kim_tools/ase/core.py
+++ kim_tools/ase/core.py
@@ -33,6 +33,7 @@ Helper routines for KIM Tests and Verification Checks
 
 import itertools
 import logging
+import math
 import random
 from typing import Union
 
@@ -41,6 +42,7 @@ from ase import Atoms
 from ase.calculators.calculator import Calculator
 from ase.calculators.kim.kim import KIM
 from ase.data import chemical_symbols
+from ase.lattice.cubic import FaceCenteredCubic
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename="kim-tools.log", level=logging.INFO, force=True)
@@ -54,6 +56,12 @@ __all__ = [
     "check_if_atoms_interacting",
     "get_isolated_energy_per_atom",
     "get_model_energy_cutoff",
+    "get_model_species_minimum_cutoff",
+    "fcc_atoms_in_supercell",
+    "generate_fcc_compute_energy",
+    "local_edge_detection",
+    "filter_good_alat",
+    "find_working_configuration_FCC",
     "fractional_coords_transformation",
     "perturb_until_all_forces_sizeable",
     "randomize_positions",
@@ -706,15 +714,62 @@ def perturb_until_all_forces_sizeable(
 
 
 ################################################################################
+def get_model_species_minimum_cutoff(
+    model: Union[str, Calculator],
+    species: list,
+    xtol: float = 1e-8,
+    etol_coarse: float = 1e-6,
+    etol_fine: float = 1e-15,
+    max_bisect_iters: int = 1000,
+    max_upper_cutoff_bracket: float = 20.0,
+) -> float:
+    """
+    Given a model and a list of species, construct all permutations of dimers and
+    compute their energy-cutoff i.e the distance at which their interaction becomes
+    non-trivial. Return the smallest such distance among all pairs of species.
+    This method calls get_model_energy_cutoff
+    """
+    min_cutoff = np.inf
+    for i in range(len(species)):
+        si: str = species[i]
+        Rii: float = get_model_energy_cutoff(
+            model,
+            [si, si],
+            xtol,
+            etol_coarse,
+            etol_fine,
+            max_bisect_iters,
+            max_upper_cutoff_bracket,
+        )
+        min_cutoff = min(min_cutoff, Rii)
+        for j in range(i + 1, len(species)):
+            sj: str = species[j]
+            try:
+                Rij: float = get_model_energy_cutoff(
+                    model,
+                    [sj, si],
+                    xtol,
+                    etol_coarse,
+                    etol_fine,
+                    max_bisect_iters,
+                    max_upper_cutoff_bracket,
+                )
+                min_cutoff = min(min_cutoff, Rij)
+            except Exception:
+                pass
+    return min_cutoff
+
+
+################################################################################
 def get_model_energy_cutoff(
-    model,
-    symbols,
-    xtol=1e-8,
-    etol_coarse=1e-6,
-    etol_fine=1e-15,
-    max_bisect_iters=1000,
-    max_upper_cutoff_bracket=20.0,
-):
+    model: Union[str, Calculator],
+    symbols: list,
+    xtol: float = 1e-8,
+    etol_coarse: float = 1e-6,
+    etol_fine: float = 1e-15,
+    max_bisect_iters: int = 1000,
+    max_upper_cutoff_bracket: float = 20.0,
+) -> float:
     """
     Compute the distance at which energy interactions become non-trival for a given
     model and a species pair it supports.  This is done by constructing a dimer composed
@@ -878,6 +933,267 @@ def get_model_energy_cutoff(
     else:
         return rcut
 
+
+################################################################################
+def generate_fcc_compute_energy(
+    model: Union[str, Calculator], species: list, alat: float, seed: int = None
+) -> tuple[float, int]:
+    """
+    Construct an FCC lattice large enough to accommodate all species,
+    evaluate its energy, and return the energy and size of the supercell.
+
+    Args:
+        model: The model name or calculator object.
+        species: List of atomic species to be incorporated into the FCC lattice.
+        alat: The lattice constant for the FCC lattice.
+        seed: The random seed for species randomization.
+
+    Returns:
+        A tuple containing:
+            - The total potential energy of the lattice.
+            - The number of unit cells along each side of the supercell.
+    """
+
+    ncells_per_side = 2
+    while True:
+        atoms = FaceCenteredCubic(
+            size=(ncells_per_side, ncells_per_side, ncells_per_side),
+            latticeconstant=alat,
+            symbol="H",
+            pbc=False,
+        )
+        if len(atoms) < len(species):
+            ncells_per_side += 1
+        else:
+            break
+    if seed is not None:
+        random.seed(seed)
+    randomize_species(atoms, species)
+    if isinstance(model, str):
+        calc = KIM(model)
+    else:
+        calc = model
+    atoms.set_calculator(calc)
+
+    # compute energy
+    try:
+        pe = atoms.get_potential_energy()
+        return pe, ncells_per_side
+    except Exception as e:
+        raise (e)
+
+
+################################################################################
+def fcc_atoms_in_supercell(n_cells_per_side: int) -> int:
+    """
+    Compute the number of atoms in an FCC supercell.
+
+    This function calculates the total number of atoms in a Face-Centered Cubic (FCC)
+    supercell based on the number of unit cells along each side. In an FCC structure,
+    each unit cell contains 4 atoms.
+
+    Args:
+        n_cells_per_side: Number of unit cells along each side of the supercell.
+                                This represents the size of the supercell.
+
+    Returns:
+        The total number of atoms in the FCC supercell.
+    """
+
+    atoms_per_unit_cell = 4
+    total_unit_cells = n_cells_per_side**3
+    total_atoms = total_unit_cells * atoms_per_unit_cell
+    return (int)(total_atoms)
+
+
+################################################################################
+def local_edge_detection(x: list, y: list) -> list:
+    """
+    Computes the Local Edge Detection (LED) values for the x-y curve.
+    The algorithm helps to identify discontinuities or abrupt changes in the curve.
+
+    Based on the paper:
+    A. Gelb and E. Tadmor, "Local edge detection for non-linear signals,"
+    Journal of Scientific Computing, 28:279-306, 2006.
+
+    Args:
+        x: A list of x-values representing the independent variable of the curve.
+        y: A list of y-values representing the dependent variable of the curve.
+
+    Returns:
+        A list of LED values of strength of discontinuities in the x-y curve.
+
+    Notes:
+        The LED[i] corresponds to the X[i+2]
+
+    """
+
+    led_values = []
+
+    fact = 1.0 / 6.0
+    for j in range(2, len(y) - 3):
+        # use 5-th order local difference formula
+        led = fact * (
+            -y[j - 2]
+            + 5 * y[j - 1]
+            - 10 * y[j]
+            + 10 * y[j + 1]
+            - 5 * y[j + 2]
+            + y[j + 3]
+        )
+        led_values.append(led)
+
+    return led_values
+
+
+################################################################################
+def filter_good_alat(
+    alats: list,
+    energies: list,
+    ncells: list,
+    leds: list,
+    min_cutoff: float,
+    etol: list = [5e-2, 5e2],
+    led_tol: float = 1.0,
+) -> dict:
+    """
+    Filter a good lattice constant (alat) based on the given criteria.
+
+    This function filters out a valid alat value from the provided lists of alats,
+    energies, number of cells, and LED values. The filtering is done based on several
+    conditions including the energy bounds, LED tolerance, and minimum cutoff distance.
+
+    Args:
+        alats: A list of lattice parameters (alat).
+        energies: A list of energy-per-atom values corresponding to each alat.
+                          Used to compute the total energy of the FCC structure
+                          for filtering.
+        ncells: A list of the number of cells corresponding to each alat.
+                        Used to compute the number of atoms for filtering by energy.
+        leds: A list of LED values corresponding to each alat.
+                     Note that leds[i] corresponds to alats[i+2]
+        min_cutoff: The minimum cutoff distance for the model-species
+                            combination. This is used to filter alats.
+        etol: A list containing the minimum and maximum energy-per-atom
+                               bounds for filtering. Default is [5e-2, 5e2].
+        led_tol: The maximum LED value for filtering. Default is 1.0.
+
+    Returns:
+        dict: A dictionary containing the filtered alat value and related properties:
+            - 'good_alat': The filtered alat value.
+            - 'min_led': The minimum LED value found.
+            - 'good_ncells': The number of cells corresponding to the selected alat.
+
+    Notes:
+        The following conditions are applied to filter the valid alat:
+        - `0.2 * min_cutoff < alat < 0.8 * min_cutoff`
+        - `etol[0] < |energy-per-atom| < etol[1]`
+        - `|LED| < led_tol`
+        - `0 < |LED| < min_led`
+
+    """
+
+    min_led = np.inf
+    good_alat = None
+    good_ncells: int = 0
+
+    N = len(alats)
+
+    for i in range(2, N - 3):
+        alat = alats[i]
+        energy = energies[i]
+        ncell = ncells[i]
+        led = leds[i - 2]  # leds[0] corresponds to alats[2]
+        natoms = fcc_atoms_in_supercell(ncell)
+
+        if alat > 0.8 * min_cutoff or alat < 0.2 * min_cutoff:
+            continue
+        if abs(energy) > etol[1] * natoms or abs(energy) < etol[0] * natoms:
+            continue
+        if abs(led) > led_tol or abs(led) > min_led:
+            continue
+
+        good_alat = alat
+        good_ncells = ncell
+        min_led = abs(led)
+
+    return {"good_alat": good_alat, "min_led": min_led, "good_ncells": good_ncells}
+
+
+################################################################################
+def find_working_configuration_FCC(
+    model: Union[str, Calculator],
+    species: list,
+    energy_bound: list = [5e-2, 5e2],
+    led_tol: float = 1.0,
+    seed: int = None,
+) -> dict:
+    """
+    Find an FCC configuration for a model and species with energy and LED constraints.
+
+    This function constructs an FCC configuration for the specified species and model,
+    and filters the configurations based on energy-per-atom and smoothness of the
+    energy-alat relation. The energy values must lie within the specified bounds,
+    and the local edge detection (LED) must be below the given tolerance.
+
+    Args:
+        model: The model name or calculator object used to
+                                        compute the energies of the configurations.
+        species: atomic species to be incorporated into the FCC structure.
+        energy_bound: A list of two values specifying the minimum
+                                       and maximum energy-per-atom bounds for filtering.
+                                       Default is [5e-2, 5e2].
+        led_tol: The tolerance for local edge detection filtering.
+                                   Default is 1.0.
+        seed: An optional random seed for reproducibility.
+
+    Returns:
+        dict: A dictionary containing the working FCC configuration, including the
+              optimal lattice constant, number of cells per side, and the
+              corresponding LED value.
+
+    Notes:
+        - The function first computes the minimum cutoff distance for the species pair.
+        - It then attempts different lattice constants and filters the configurations
+          based on energy-per-atom and LED values.
+        - Only configurations that meet the energy and LED criteria are retained.
+
+    """
+
+    min_cutoff = get_model_species_minimum_cutoff(model, species)
+    amax = 1.0 * min_cutoff
+    amin = 0.3 * min_cutoff
+    del_a = 0.01
+    na = int(math.ceil((amax - amin) / del_a))
+
+    alats = []
+    energies = []
+    ncells = []  # will be used for filtering by energy-per-atom
+
+    for j in range(0, na + 1):
+        a = amin + j * del_a
+        try:
+            val = None
+            if seed is None:
+                val = generate_fcc_compute_energy(model, species, a)
+            else:
+                val = generate_fcc_compute_energy(model, species, a, seed)
+            if val is not None:
+                alats.append(a)
+                energies.append(val[0])  # first value is energy
+                ncells.append(val[1])  # second value is number of atoms
+
+        except Exception:
+            continue
+
+    # these LED values correspond to alats[2] ... alats[na-3]
+    leds = local_edge_detection(alats, energies)
+    return filter_good_alat(
+        alats, energies, ncells, leds, min_cutoff, energy_bound, led_tol
+    )
+
+
+################################################################################
 
 # If called directly, do nothing
 if __name__ == "__main__":
