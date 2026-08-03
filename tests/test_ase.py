fe0046bde091dc8a2e803d207a48ae6c57fa2938
nav-mohan <mohan227@umn.edu>
2026-08-03 13:56:03 -0500

    improved unit-testing for FEC
    test for timeout failure results
    test for fallback

--- tests/test_ase.py
+++ tests/test_ase.py
@@ -33,7 +33,24 @@ def test_find_equilibrium_configuration_FCC():
     assert np.isclose(result["mono_species_equilibrium_alats"]["Al"], 3.3281, atol=1e-4)
     assert np.isclose(result["mono_species_equilibrium_alats"]["Au"], 3.7407, atol=1e-4)
     assert np.isclose(result["equilibrium_alat"], 3.5630, atol=1e-4)
+    assert np.isclose(result["approx_mixed_equilibrium_alat"], 3.5344, atol=1e-4)
     assert result["ncells_per_side"] == 1
+
+
+# end-to-end test (will generate an FCC config larger than default)
+def test_find_equilibrium_configuration_FCC_n_cells_per_side():
+    model = "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003"
+    species = ["H", "C", "N", "O", "S"]
+    result = find_equilibrium_config_FCC(model, species)
+
+    assert np.isclose(result["mono_species_equilibrium_alats"]["H"], -1, atol=1e-4)
+    assert np.isclose(result["mono_species_equilibrium_alats"]["C"], 2.0904, atol=1e-4)
+    assert np.isclose(result["mono_species_equilibrium_alats"]["N"], 1.9528, atol=1e-4)
+    assert np.isclose(result["mono_species_equilibrium_alats"]["O"], 1.8153, atol=1e-4)
+    assert np.isclose(result["mono_species_equilibrium_alats"]["S"], 2.8880, atol=1e-4)
+    assert np.isclose(result["equilibrium_alat"], 2.4201, atol=1e-4)
+    assert np.isclose(result["approx_mixed_equilibrium_alat"], 2.1866, atol=1e-4)
+    assert result["ncells_per_side"] == 2
 
 
 # expected to fail due to coarse-scanning timeout
@@ -45,7 +62,10 @@ def test_find_equilibrium_configuration_FCC_coarseTimeout():
     assert result["mono_species_equilibrium_alats"]["Al"] == -1.0
     assert result["mono_species_equilibrium_alats"]["Au"] == -1.0
     assert result["equilibrium_alat"] == -1.0
-    assert result["ncells_per_side"] == 1
+    assert np.isclose(result["approx_mixed_equilibrium_alat"], -1.0, atol=1e-4)
+    assert result["mono_species_results"][0]["reference_config"]["ncells_per_side"] == 1
+    assert result["mono_species_results"][1]["reference_config"]["ncells_per_side"] == 1
+    assert result["mixed_species_result"]["reference_config"]["ncells_per_side"] == 1
 
 
 # expected to fail due to nelder-mead timeout
@@ -57,4 +77,7 @@ def test_find_equilibrium_configuration_FCC_nelderMeadTimeout():
     assert result["mono_species_equilibrium_alats"]["Al"] == -1.0
     assert result["mono_species_equilibrium_alats"]["Au"] == -1.0
     assert result["equilibrium_alat"] == -1.0
-    assert result["ncells_per_side"] == 1
+    assert np.isclose(result["approx_mixed_equilibrium_alat"], -1.0, atol=1e-4)
+    assert result["mono_species_results"][0]["reference_config"]["ncells_per_side"] == 1
+    assert result["mono_species_results"][1]["reference_config"]["ncells_per_side"] == 1
+    assert result["mixed_species_result"]["reference_config"]["ncells_per_side"] == 1
