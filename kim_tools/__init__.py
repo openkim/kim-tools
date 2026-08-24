24b4d7133ef0b08848b880c70827401c085828d1
Claire Waters <bwaters@umn.edu>
2026-08-24 15:39:53 -0500

    STY: run pre-commit

--- kim_tools/__init__.py
+++ kim_tools/__init__.py
@@ -4,14 +4,14 @@ from .aflow_util import *
 from .aflow_util import __all__ as aflow_all
 from .ase import *
 from .ase import __all__ as ase_all
+from .excerpts import *
+from .excerpts import __all__ as excerpts_all
 from .symmetry_util import *
 from .symmetry_util import __all__ as symmetry_all
 from .test_driver import *
 from .test_driver import __all__ as test_driver_all
 from .vc import *
 from .vc import __all__ as vc_all
-from .excerpts import *
-from .excerpts import __all__ as excerpts_all
 
 # TODO: Go through all other TODOs in code and make sure they've been
 # turned into issues
--- tests/test_units.py
+++ tests/test_units.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from kim_tools.excerpts.kimunits import convert_list, convert_units, add_si_units
+from kim_tools.excerpts.kimunits import add_si_units, convert_list, convert_units
 
 
 def test_units() -> None:
@@ -9,4 +9,4 @@ def test_units() -> None:
     assert one_kilogram[1] == "kg"
     assert np.isclose(convert_units(0, "tempC", "tempF", suppress_unit=True), 32)
     assert np.allclose(convert_list([1000, 1000], "g")[0], [1, 1])
-    assert np.isclose(add_si_units({"source-value":1000,"source-unit":"mm"})[0], 1)
+    assert np.isclose(add_si_units({"source-value": 1000, "source-unit": "mm"})[0], 1)
